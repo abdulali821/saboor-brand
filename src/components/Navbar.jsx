cff19711e6c2eb6794c90d2bd73c5cd9f63cd109
abdulali821 <abdulali821@users.noreply.github.com>
2022-07-18 18:48:00 +0500

    comit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id='navbar'>
+        <div className='container'>
+            <h1>
+                AS.
+            </h1>
+            <ul>
+                <li><a href="#showcase">HOME</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#feedback">FEEDBACK</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Showcase.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Showcase = () => {
+  return (
+        <div id='showcase'>
+            <div className="container">
+                <div className="showcase-text">
+                    <h1>Raining Offers For Hot Summer!</h1>
+                    <h3>25% Off On All Products</h3>
+                    <div className="btn">
+                        <button>Shop Now</button>
+                        <button>View More</button>
+                    </div>
+                </div>
+                <div className="showcase-image">
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Showcase
