780d0881fc1b3513f75883ca71d0080a9756f13e
abdulali821 <abdulali821@users.noreply.github.com>
2022-07-19 15:36:14 +0500

    comit

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from "react";
+import CardItem from "./CardItem";
 
 const CardList = () => {
-    let array=[
-        {
-            name: 'Ittehad',
-            id: 1,
-            price: 1000
-        },
-        {
-            name: 'Ittehad',
-            id: 2,
-            price: 1000
-        },
-        {
-            name: 'Ittehad',
-            id: 3,
-            price: 1000
-        },
-        {
-            name: 'Ittehad',
-            id: 4,
-            price: 1000
-        },
-        {
-            name: 'Ittehad',
-            id: 5,
-            price: 1000
-        },
-    ]
+  let array = [
+    {
+      name: "Ittehad",
+      id: 1,
+      price: 1000,
+    },
+    {
+      name: "Ittehad",
+      id: 2,
+      price: 1000,
+    },
+    {
+      name: "Ittehad",
+      id: 3,
+      price: 1000,
+    },
+    {
+      name: "Ittehad",
+      id: 4,
+      price: 1000,
+    },
+    {
+      name: "Ittehad",
+      id: 5,
+      price: 1000,
+    },
+  ];
   return (
-    <div className='cardList'>
-        {array.map((card)=>(
-            <CardItem key={card.id} name={card.name} id={card.id} price={card.price}/>
-        ))}
+    <div className="cardList">
+      {array.map((card) => (
+        <CardItem
+          key={card.id}
+          name={card.name}
+          id={card.id}
+          price={card.price}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
